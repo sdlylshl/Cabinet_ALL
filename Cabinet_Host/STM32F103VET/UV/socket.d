1656b0b0a0a94587d562d9a74c1ea06d6ba28413
sdlylshl <sdlylshl@163.com>
2014-11-26 16:26:40 +0800

    V2.3 删除部分无用文件 优化gitignore

--- Cabinet_Host/STM32F103VET/UV/socket.d
+++
@@ -1,6 +0,0 @@
-.\socket.o: ..\Source\W5500\Ethernet\socket.c
-.\socket.o: ..\Source\W5500\Ethernet\socket.h
-.\socket.o: ..\Source\W5500\Ethernet\wizchip_conf.h
-.\socket.o: D:\Program Files\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
-.\socket.o: ..\Source\W5500\Ethernet\W5500/w5500.h
-.\socket.o: ..\Source\W5500\Ethernet\W5500/../wizchip_conf.h
--- Cabinet_Host/STM32F103VET/UV/w5500.d
+++
@@ -1,6 +0,0 @@
-.\w5500.o: ..\Source\W5500\Ethernet\W5500\w5500.c
-.\w5500.o: D:\Program Files\Keil_v5\ARM\ARMCC\bin\..\include\stdio.h
-.\w5500.o: ..\Source\W5500\Ethernet\W5500\w5500.h
-.\w5500.o: D:\Program Files\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
-.\w5500.o: ..\Source\W5500\Ethernet\W5500\../wizchip_conf.h
-.\w5500.o: ..\Source\W5500\Ethernet\W5500\../W5500/w5500.h
